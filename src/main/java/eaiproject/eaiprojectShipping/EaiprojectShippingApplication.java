142f4fae0cf637838716ebc0d14e08815a768d1b
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-10-15 10:39:11 +0200

    Init

---
+++ src/main/java/eaiproject/eaiprojectShipping/EaiprojectShippingApplication.java
@@ -0,0 +1,12 @@
+package eaiproject.eaiprojectShipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiprojectShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiprojectShippingApplication.class, args);
+	}
+}
