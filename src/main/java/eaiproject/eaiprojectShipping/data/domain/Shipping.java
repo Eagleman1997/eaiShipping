32889f34949842ec2db2fd7da525aa013f9968f2
Lukas <Lukas@DESKTOP-1BU7COR.edu.ds.fhnw.ch>
2018-10-15 17:01:46 +0200

    entities added

--- src/main/java/eaiproject/eaiprojectShipping/data/domain/Shipping.java
+++ src/main/java/eaiproject/eaiprojectShipping/data/domain/Shipping.java
@@ -1,10 +1,20 @@
 package eaiproject.eaiprojectShipping.data.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Shipping {
+	
+	@Id @GeneratedValue
+	private Integer tracking_id;
+	private Integer order_id;
+	private String shipping_address_name;
+	private String shipping_address_street;
+	private String shipping_address_location;
+	private String parcel_service;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Shipping(){
+		super();
 	}
 
 }
