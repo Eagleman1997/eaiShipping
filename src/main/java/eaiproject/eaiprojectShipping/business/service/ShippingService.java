102a81993bd01a2dfb575346ccf91e6eb59395d0
Lukas <Lukas@DESKTOP-PC5RKCS>
2018-11-18 10:10:02 +0100

    event processing

--- src/main/java/eaiproject/eaiprojectShipping/business/service/ShippingService.java
+++ src/main/java/eaiproject/eaiprojectShipping/business/service/ShippingService.java
@@ -1,6 +1,9 @@
 package eaiproject.eaiprojectShipping.business.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import eaiproject.eaiprojectShipping.data.domain.Shipping;
 import eaiproject.eaiprojectShipping.data.repository.ShippingRepository;
@@ -9,6 +12,20 @@ public class ShippingService {
 	
 	@Autowired
 	private ShippingRepository shippingRepository;
+	
+    private Logger logger = LoggerFactory.getLogger(ShippingService.class);
+
+    public Shipping shipGoods(Integer orderId, String shipping_address_name, String shipping_address_street, String shipping_address_location, String parcel_service) throws Exception {
+        logger.info("shipGoods() with orderId " + orderId + " and shipping_adress " + shipping_address_street + " " + shipping_address_location + " called and hand over the parcel to the delivery service " + parcel_service);
+        for(long seconds = 5; seconds > 0; seconds--) {
+            logger.info("Delivery service ready in " + seconds + " seconds");
+            Thread.sleep(1000);
+        }
+        // ...
+        Shipping shipping = new Shipping(orderId, shipping_address_name, shipping_address_street, shipping_address_location, parcel_service);
+        logger.info("Packet transferred to delivery service and tracking number "+ shipping.getOrder_id() + " received");
+        return shipping;
+    }
 	
 	public Shipping createShipping(Integer orderId, String shipping_address_name, String shipping_address_street, String shipping_address_location, String parcel_service) {
 		Shipping shipping = new Shipping(orderId, shipping_address_name, shipping_address_street, shipping_address_location, parcel_service);
